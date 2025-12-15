7a1ad3881a47ef2418ad4c6d5ad5139a0a0a7bed
kihochalong <41330247@nfu.edu.tw>
2025-12-15 15:58:42 +0800

    project deploy

---
+++ imageprocessor.h
@@ -0,0 +1,36 @@
+#ifndef IMAGEPROCESSOR_H
+#define IMAGEPROCESSOR_H
+
+#include <QMainWindow>
+#include <QAction>
+#include <QMenu>
+#include <QToolBar>
+#include <QImage>
+#include <QLabel>
+
+#include <QMainWindow>
+
+class ImageProcessor : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ImageProcessor(QWidget *parent = nullptr);
+    ~ImageProcessor();
+    void createActions();
+    void createMenus();
+    void createToolBars();
+    void loadFile(QString filename);
+private slots:
+    void showOpenFile();
+private:
+    QWidget   *central;
+    QMenu     *fileMenu;
+    QToolBar  *fileTool;
+    QImage     img;
+    QString    filename;
+    QLabel    *imgWin;
+    QAction   *openFileAction;
+    QAction   *exitAction;
+};
+#endif // IMAGEPROCESSOR_H
